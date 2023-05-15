a5e13c63a7fe382569a7a05a01e4b359df941835
Saumya Mundra <saumyamundra@gmail.com>
2023-05-15 20:28:59 +0530

    added one more command to move file

--- data/moving_mnist/download_mmnist.sh
+++ data/moving_mnist/download_mmnist.sh
@@ -1,3 +1,4 @@
 wget http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy
 wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
 mv train-images-idx3-ubyte.gz data/moving_mnist/
+mv mnist_test_seq.npy data/moving_mnist/
