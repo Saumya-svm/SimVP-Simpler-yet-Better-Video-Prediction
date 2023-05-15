0a647411d8bacd4bfdbd8b099b1493f5b8da6254
Saumya Mundra <saumyamundra@gmail.com>
2023-05-15 20:27:52 +0530

    moved gz file to location mentioned for training

--- data/moving_mnist/download_mmnist.sh
+++ data/moving_mnist/download_mmnist.sh
@@ -1,2 +1,3 @@
 wget http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy
 wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+mv train-images-idx3-ubyte.gz data/moving_mnist/
