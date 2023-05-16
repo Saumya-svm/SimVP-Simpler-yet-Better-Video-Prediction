d9a0efa8ba14c263859483861a39deab36d97914
Saumya Mundra <saumyamundra@gmail.com>
2023-05-17 00:15:17 +0530

    script to setup data for windpmf training

---
+++ data/windpmf/download_windpmf.sh
@@ -0,0 +1,5 @@
+wget --content-disposition https://iith-my.sharepoint.com/:u:/g/personal/ch20btech11031_iith_ac_in/EcWYlAZ_YvlLguVpkBnzVcUB9ErXgVxFjuMoGX9J6pNqdA?download=1
+mv 'tcnn data.zip' tcnn_data.zip
+unzip tcnn_data.zip
+mv 'tcnn data' tcnn_data
+mkdir tcnn_images
