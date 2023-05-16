11fac42c6461f9a51ca9acc3060508a11f680398
Saumya Mundra <saumyamundra@gmail.com>
2023-05-17 00:17:20 +0530

    added script for taxi bj data for training

---
+++ data/taxibj/download_taxibj.sh
@@ -0,0 +1,4 @@
+wget --content-disposition https://iith-my.sharepoint.com/:u:/g/personal/ch20btech11031_iith_ac_in/ESBJTHJ-tP5FvzlLBbEOzwgBf-39E24SN_18rK8icagefQ?download=1
+mkdir data/taxibj/
+mv TrafficBJ.zip data/taxibj/
+unzip data/taxibj/TrafficBJ.zip -d data/taxibj/
